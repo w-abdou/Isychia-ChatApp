0f1fd2dda25fb66f22fad7e3f20b63360e668cc5
Worood Abdou <w@pravica.io>
2025-03-27 10:47:01 +0200

    Modifications

--- Test Functions/functions/block_unblock.java
+++
@@ -1,8 +0,0 @@
-public void blockUser(User targetUser) {
-    System.out.println(username + " has blocked " + targetUser.username);
-}
-
-public void unblockUser(User targetUser) {
-    System.out.println(username + " has unblocked " + targetUser.username);
-}
-
